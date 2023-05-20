3c4a4af7896db018603213a2a8beb50564696c56
Aaron Hu <a.k.hu@wustl.edu>
2023-05-19 19:34:16 -0500

    fun

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
-from kivy.app import App
-from kivy.uix.camera import Camera
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+import cv2 
 
-class cam(App):
-    def build(self):
-        '''
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+while True:
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'): 
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+            img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+            img_new = cv2.imshow("Captured Image", img_new)
+            cv2.waitKey(1650)
+            cv2.destroyAllWindows()
+            print("Processing image...")
+            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+            print("Converting RGB image to grayscale...")
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+            edged = cv2.Canny(blurred, 30, 150)
+            print("Converted RGB image to grayscale...")
+            print("Resizing image to 28x28 scale...")
+            img_ = edged
+            print("Resized...")
+            img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+            print("Image saved!")
         
-        :return:
-        '''
-        self.camera_obj = Camera()
-        self.camera_obj.resolution = (800,800)
-
-
-        button_obj = Button(text = "press")
-        button_obj.size_hint(.5,.2)
-        button_obj.pos_hint = {'x':25, 'y':.25}
-        button_obj(on_press = self.shutter)
-
-        layout = BoxLayout()
-        layout.add_widget(self.camera_obj)
-        layout.add_widget(button_obj)
-        return layout
+            break
+        elif key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
     
-    def shutter(self, *args):
-        self.camera_obj.export_to_png("./shutter.png")
-if __name__ == '__main__':
-    cam().run()
